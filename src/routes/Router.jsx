9bba41e3c17058df759b3820d0443aa6bc96ce48
omerbenda98 <omerbenda98@gmail.com>
2023-11-17 13:08:58 +0200

    test3

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -18,72 +18,74 @@ import BreedPage from "../pages/BreedPage";
 import ChatPage from "../pages/ChatPage";
 import DonationPage from "../pages/DonationPage";
 
-const Router = () => {
+const AppRouter = () => {
   return (
-    <Router>
-      <Route path={ROUTES.HOME} element={<HomePage />} />
-      <Route path={ROUTES.ABOUT} element={<AboutPage />} />
-      <Route path={ROUTES.FAKEHOME} element={<Navigate to={ROUTES.HOME} />} />
-      <Route path="/breed/:breed" element={<BreedPage />} />
-      <Route path="/chats/:tokenId/:userId" element={<ChatPage />} />
-      <Route path={ROUTES.ADOPTION} element={<AdoptionPage />} />
-      <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
-      <Route path={ROUTES.DONATION} element={<DonationPage />} />
-      <Route
-        path={ROUTES.MYCARDS}
-        element={
-          <SuperProtectedRoute
-            isAdmin={false}
-            isBiz={true}
-            element={<MyCardsPage />}
-          />
-        }
-      />
+    <HashRouter>
+      <Router>
+        <Route path={ROUTES.HOME} element={<HomePage />} />
+        <Route path={ROUTES.ABOUT} element={<AboutPage />} />
+        <Route path={ROUTES.FAKEHOME} element={<Navigate to={ROUTES.HOME} />} />
+        <Route path="/breed/:breed" element={<BreedPage />} />
+        <Route path="/chats/:tokenId/:userId" element={<ChatPage />} />
+        <Route path={ROUTES.ADOPTION} element={<AdoptionPage />} />
+        <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
+        <Route path={ROUTES.DONATION} element={<DonationPage />} />
+        <Route
+          path={ROUTES.MYCARDS}
+          element={
+            <SuperProtectedRoute
+              isAdmin={false}
+              isBiz={true}
+              element={<MyCardsPage />}
+            />
+          }
+        />
 
-      <Route
-        path={ROUTES.CRM}
-        element={
-          <SuperProtectedRoute
-            isAdmin={true}
-            isBiz={true}
-            element={<CRMpage />}
-          />
-        }
-      ></Route>
-      <Route path={ROUTES.CREATE} element={<CreateCardPage />} />
-      <Route path={ROUTES.LOGIN} element={<LoginPage />} />
-      <Route
-        path={ROUTES.LOGOUT}
-        element={<ProtectedRoute element={<LogoutPage />} />}
-      />
-      <Route
-        path="/edit/:id"
-        element={
-          <SuperProtectedRoute
-            isAdmin={false}
-            isBiz={true}
-            element={<EditCardPage />}
-          />
-        }
-      />
-      <Route
-        path={ROUTES.PROFILE}
-        element={<ProtectedRoute element={<ProfilePage />} />}
-      />
-      <Route
-        path={ROUTES.CREATE}
-        element={
-          <SuperProtectedRoute
-            isAdmin={false}
-            isBiz={true}
-            element={<CreateCardPage />}
-          />
-        }
-      />
+        <Route
+          path={ROUTES.CRM}
+          element={
+            <SuperProtectedRoute
+              isAdmin={true}
+              isBiz={true}
+              element={<CRMpage />}
+            />
+          }
+        ></Route>
+        <Route path={ROUTES.CREATE} element={<CreateCardPage />} />
+        <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+        <Route
+          path={ROUTES.LOGOUT}
+          element={<ProtectedRoute element={<LogoutPage />} />}
+        />
+        <Route
+          path="/edit/:id"
+          element={
+            <SuperProtectedRoute
+              isAdmin={false}
+              isBiz={true}
+              element={<EditCardPage />}
+            />
+          }
+        />
+        <Route
+          path={ROUTES.PROFILE}
+          element={<ProtectedRoute element={<ProfilePage />} />}
+        />
+        <Route
+          path={ROUTES.CREATE}
+          element={
+            <SuperProtectedRoute
+              isAdmin={false}
+              isBiz={true}
+              element={<CreateCardPage />}
+            />
+          }
+        />
 
-      <Route path="*" element={<h1>404</h1>} />
-    </Router>
+        <Route path="*" element={<h1>404</h1>} />
+      </Router>
+    </HashRouter>
   );
 };
 
-export default Router;
+export default AppRouter;
