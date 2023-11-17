523a9e64e89982bbb973e225277160b5f2f5fab5
omerbenda98 <omerbenda98@gmail.com>
2023-11-17 14:14:32 +0200

    url fix

--- src/pages/DonationPage.jsx
+++ src/pages/DonationPage.jsx
@@ -9,21 +9,21 @@ const charities = [
     "Thailand",
     "Soi Dog Foundation (Soi Dog) was established in 2003 in Phuket, Thailand, to help the street dogs and cats who had no-one else to care for them. Over 70,000 strays roamed the island, with the numbers growing alarmingly due to a lack of spay and neuter programmes to control the population. Soi Dog was created to provide a humane and sustainable solution to managing the stray population and to address their medical needs. Funding then, as it does now, came entirely from individuals who shared, and continue to share, the vision of our founders.",
     "https://www.soidog.org/",
-    "./ASPCA.jpg",
+    "./charities-images/ASPCA.jpg",
   ],
   [
     "ASPCA",
     "Italy",
     "The American Society for the Prevention of Cruelty to Animals® (ASPCA®) was the first humane society to be established in North America and is, today, one of the largest in the world. Our organization was founded on the belief that animals are entitled to kind and respectful treatment at the hands of humans and must be protected under the law. Headquartered in New York City, the ASPCA maintains a strong local presence, and with programs that extend our anti-cruelty mission across the country, we are recognized as a national animal welfare organization.”",
     "https://www.aspca.org/",
-    "./soidog.jpg",
+    "./charities-images/soidog.jpg",
   ],
   [
     "AllDogsMatter",
     "UK",
     "All Dogs Matter is a dog rescue and rehoming charity working in and around London to transform the lives of unwanted and abandoned dogs. We also rehome dogs in need from overseas. In 2022 All Dogs Matter rescued and rehomed 349 dogs with new owners.",
     "https://alldogsmatter.co.uk/",
-    "./allDogsMatter.jpg",
+    "./charities-images/allDogsMatter.jpg",
   ],
 ];
 
