cfa713de60c487ff1fc4518a0532e91f50cc212d
omerbenda98 <omerbenda98@gmail.com>
2024-08-05 15:11:06 +0300

    test

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+import { Provider } from "react-redux";
+import store from "../store/bigPie";
+import { HashRouter as Router } from "react-router-dom";
+
+test("renders DogHome link", () => {
+  const { container } = render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
+  );
+  console.log(container.innerHTML); // Log the output
+  const linkElement = screen.getByText(/DogHome/i);
+  expect(linkElement).toBeInTheDocument();
+});
